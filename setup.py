8f76ab3b72a0d921784f35e6429a3a5d865de3b6
cyrille dibi <dkonancyrille@gmail.com>
2021-12-17 16:18:51 +0100

    update package version from 0.0.1 to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pkg_temperature_convrsn",
-    version="0.0.1",
+    version="0.0.2",
     author="konan",
     author_email="author@example.com",
     description="A small package to converte temperat",
